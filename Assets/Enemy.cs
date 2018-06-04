9bfa1a9ef240c0e9049d4bcb0bf618d164db1a39
Duli <ablais147@gmail.com>
2018-06-03 19:47:58 -1000

    Completed assignment 2-1

---
+++ Assets/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GameManager.instance.enemies.Add(this); // add enemy to list on start of game
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDestroy() //function for what happens when an enemy is destroyed
+    {
+        GameManager.instance.enemies.Remove(this);
+    }
+}
+
+
+
